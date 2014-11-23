b2fe25e564ca774d3e982791fc09dcc9a21dd348
Lowell Bass <lwb4@live.com>
2014-11-23 02:47:31 -0500

    Progress 11/23/2014 2:47

--- CollabApp/app/src/main/java/com/example/riderx2/collabapp/AvailabiltyScreen.java
+++ CollabApp/app/src/main/java/com/example/riderx2/collabapp/AvailabiltyScreen.java
@@ -1,20 +1,46 @@
 package com.example.riderx2.collabapp;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.widget.TextView;
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.List;
+        import java.util.Map;
 
+        import org.apache.http.HttpResponse;
+        import org.apache.http.client.ClientProtocolException;
+        import org.apache.http.client.HttpClient;
+        import org.apache.http.client.methods.HttpPost;
+        import org.apache.http.impl.client.DefaultHttpClient;
+        import org.json.JSONArray;
+        import org.json.JSONException;
+        import org.json.JSONObject;
+
+        import android.os.AsyncTask;
+        import android.os.Bundle;
+        import android.app.Activity;
+        import android.view.Menu;
+        import android.widget.ListView;
+        import android.widget.SimpleAdapter;
+        import android.widget.Toast;
 
 public class AvailabiltyScreen extends Activity {
     private TextView testText;
+    private String jsonResult;
+    private String url = "http://localhost/appcollab/index.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_availabilty_screen);
         testText = (TextView)findViewById(R.id.textView);
-        testText.setText();
+        testText.setText(jsonResult);
     }
 
 
@@ -24,6 +50,76 @@ public class AvailabiltyScreen extends Activity {
         getMenuInflater().inflate(R.menu.availabilty_screen, menu);
         return true;
     }
+    // Async Task to access the web
+    private class JsonReadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(params[0]);
+            try {
+                HttpResponse response = httpclient.execute(httppost);
+                jsonResult = inputStreamToString(
+                        response.getEntity().getContent()).toString();
+            }
+
+            catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        private StringBuilder inputStreamToString(InputStream is) {
+            String rLine = "";
+            StringBuilder answer = new StringBuilder();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+
+            try {
+                while ((rLine = rd.readLine()) != null) {
+                    answer.append(rLine);
+                }
+            }
+
+            catch (IOException e) {
+                // e.printStackTrace();
+                Toast.makeText(getApplicationContext(),
+                        "Error..." + e.toString(), Toast.LENGTH_LONG).show();
+            }
+            return answer;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            ListDrawer();
+        }
+
+    }// end async task
+    public void accessWebService() {
+        JsonReadTask task = new JsonReadTask();
+        // passes values for the urls string array
+        task.execute(new String[] { url });
+    }
+    public void ListDrawer() {
+       // List<Map<String, String>> employeeList = new ArrayList<Map<String, String>>();
+
+        try {
+            JSONObject jsonResponse = new JSONObject(jsonResult);
+            JSONArray jsonMainNode = jsonResponse.optJSONArray("users");
+            String outPut= "";
+            for (int i = 0; i < jsonMainNode.length(); i++) {
+                JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
+                String name = jsonChildNode.optString("UserFirst");
+                String number = jsonChildNode.optString("UserID");
+                outPut = outPut+name + "-" + number+" ";
+
+            }
+        } catch (JSONException e) {
+            Toast.makeText(getApplicationContext(), "Error" + e.toString(),
+                    Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
